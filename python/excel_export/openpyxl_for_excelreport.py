b1ba29b3f83ab0c9b00faa400b3de4382c889563
Hyungyu <qqkqqk23@naver.com>
2019-07-15 17:18:23 +0900

    openpyxl 정리중

--- python/excel_export/openpyxl_for_excelreport.py
+++ python/excel_export/openpyxl_for_excelreport.py
@@ -15,33 +15,58 @@
 
 # <Example Select Code>
 from openpyxl import Workbook
-from openpyxl.styles import Font, Alignment
-import numpy as np
+from openpyxl.styles import Font, Alignment, Border, Side, PatternFill, Color, GradientFill
+from openpyxl.utils.dataframe import dataframe_to_rows
+import pandas as pd
 
 file_nm = "excel-test.xlsx"
 
 wb = Workbook()
-ws = wb.create_sheet("Qsheet1",0)  # Qsheet1 이름의 시트를 1번째 위치에 생성
-ws2 = wb.create_sheet("Qsheet2",1)  # Qsheet2 이름의 시트를 2번째 위치에 생성
+ws_report = wb.create_sheet("Report",0)  # Qsheet1 이름의 시트를 1번째 위치에 생성
+ws_data = wb.create_sheet("Data",1)  # Qsheet2 이름의 시트를 2번째 위치에 생성
 # 위치를 건너뛰고 생성했을 경우 sheet이름의 중산 시트가 1개 생성되고 설정 이름의 시트가 생성된다. 
 # 순서가 잘못되면 원하는 위치로 생성되지 않는 것으로 보임.
 # ex)ws = wb.create_sheet("Qsheet5",5)  -> 생성시트 (Qsheet1, Qsheet2, sheet,  Qsheet5)
 
-# test_data = np.array([[1,2,3],
-#             [4,5,6],
-#             [7,8,9]])
+######################### Report sheet #########################
+# cell for title 
+ws_report.merge_cells('A1:F1')
+ws_report['A1'] = 'Report'
+title = ws_report['A1']
 
-# print(test_data.shape)
+title.fill = PatternFill("solid", fgColor="DDDDDD")
+title.font = Font(name='맑은 고딕', size = 15, bold=True)
+title.alignment = Alignment(horizontal='center', vertical='center')
 
-# for row_index in range 
-
-# cell 작업
-ws.merge_cells('B2:F2')
-ws['B2'] = '분석 결과'
+#################################################################
 
 
+######################### Data sheet #########################
+# cell for title 
+ws_data.merge_cells('A1:F1')
+ws_data['A1'] = 'Data'
+title = ws_data['A1']
 
-# sheet 삭제
+title.fill = PatternFill("solid", fgColor="DDDDDD")
+title.font = Font(name='맑은 고딕', size = 15, bold=True)
+title.alignment = Alignment(horizontal='center', vertical='center')
+
+# cell for dataframe data
+data = pd.DataFrame([[1,2,3],
+            [4,5,6],
+            [7,8,9]])
+
+for r in dataframe_to_rows(data, index=False, header=True):
+    ws_data.append(r)
+
+###############################################################
+
+
+
+### sheet 삭제 ###
 # wb.remove_sheet(ws)
-# wb.save(file_nm)
+
+
+wb.save(file_nm)
+wb.close()
 
